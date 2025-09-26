b963ef155d0f197c9834e434ee7d5c767bb74297
mayangtaray69-ui <mayangtaray69@gmail.com>
2025-09-26 14:03:09 +0800

    Messenger

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
+
 import React from "react";
 import { ScrollView } from "react-native";
-import CounterApp from "./CounterApp";
-import ColorChangerApp from "./ColorChangerApp";
+import Messenger from "./Messenger";
 
 export default function App() {
   return (
     <ScrollView style={{ flex: 1 }}>
-      <CounterApp/> 
-      <ColorChangerApp />
-
+      <Messenger/> 
 
     </ScrollView>
   );
